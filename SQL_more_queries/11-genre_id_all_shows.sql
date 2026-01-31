e2823228dec754fef51b34a18e4f9b0663ffd8be
EmilianoRodriguez78 <emiliano16rod@gmail.com>
2026-01-31 00:47:36 -0600

    11-genre_id_all_shows.sql

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,8 +1,7 @@
--- Lists all shows contained in the database hbtn_0d_tvshows.
--- Each record displays: tv_shows.title - tv_show_genres.genre_id
--- Results are sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
--- If a show doesn't have a genre, it displays NULL
+-- Script que lista todos los shows y sus IDs de género
+-- Usa LEFT JOIN para incluir shows sin género (NULL)
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+  FROM tv_shows
+  LEFT JOIN tv_show_genres
+    ON tv_shows.id = tv_show_genres.show_id
+ ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
