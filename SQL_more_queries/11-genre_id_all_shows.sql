aaea9d828abdadf012308582b23e78075179d179
EmilianoRodriguez78 <emiliano16rod@gmail.com>
2026-01-31 00:53:14 -0600

    11-genre_id_all_shows.sql

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,8 +1,8 @@
 -- Script que lista todos los shows y sus IDs de género
 -- Usa LEFT JOIN para incluir shows sin género (NULL)
-SELECT title, genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
-ORDER BY title, genre_id;
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
 
