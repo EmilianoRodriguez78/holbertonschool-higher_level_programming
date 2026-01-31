f78c0ae641332cc12b6b955c3034d5e9ae0fe8cf
EmilianoRodriguez78 <emiliano16rod@gmail.com>
2026-01-31 00:03:40 -0600

    Update 11-genre_id_all_shows.sql

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
--- Lists all shows contained in the database hbtn_0d_tvshows
--- Displays NULL for shows without genres
+-- Lista todos los shows de la base de datos hbtn_0d_tvshows
+-- Si un show no tiene género, muestra NULL
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
