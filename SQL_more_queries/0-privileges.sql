a64fd9d152075da95c20d62b1379cd323ca46bf1
EmilianoRodriguez78 <emiliano16rod@gmail.com>
2026-01-30 22:48:28 -0600

    Update 0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
---Write a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your serve
+-- Lists all privileges of user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- Lists all privileges of user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
